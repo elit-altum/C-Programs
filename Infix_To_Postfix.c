0d612d3e267f91abe5572b27e51c54886f43ebfc
elit-altum <41413622+elit-altum@users.noreply.github.com>
2019-02-25 01:21:17 +0530

    Infix expression to postfix expression
    Converts the infix expression given by the user to it's equivalent postfix expression and prints to the console.
    
    Written in ANSI-C for the GNU-GCC complier
    Elit Altum

---
+++ Infix_To_Postfix.c
@@ -0,0 +1,67 @@
+//To convert a given infix expression from the user to postfix using stacks
+//12.02.19
+
+#include<stdio.h>
+
+//Decalre and initialize stack in global scope
+
+char stack[20];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+ 
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+ 
+int priority(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+}
+ 
+main()
+{
+    char exp[20];
+    char *e, x;
+    printf("Enter the expression :: ");
+    scanf("%s",exp);
+    e = exp;
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c", x);
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c",pop());
+            push(*e);
+        }
+        e++;
+    }
+    while(top != -1)
+    {
+        printf("%c",pop());
+    }
+}
+
+//Written in ANSI for the GNU-GCC complier
+//Elit Altum
